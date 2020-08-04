c0ccb9e1b27785361510d17a5fe31af2f8928518
celinashen <celina.shen06@gmail.com>
2020-08-04 09:43:31 -0400

    Update UserDataController.java

--- search-api/src/main/java/com/example/searchapi/UserDataController.java
+++ search-api/src/main/java/com/example/searchapi/UserDataController.java
@@ -1,7 +1,6 @@
 package com.example.searchapi;
 
 import com.example.searchapi.ResourceNotFoundException;
-//import com.example.searchapi.exception.ResourceNotFoundException;
 import com.example.searchapi.UserData;
 import com.example.searchapi.UserDataRepository;
 
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,7 +25,6 @@ public class UserDataController {
     UserDataRepository userDataRepository;
 
     @Autowired
-    //UserSpecification userSpec = new UserSpecification();
     UserService userSpec;
 
     UserSearchCriteria search;
@@ -42,27 +39,6 @@ public class UserDataController {
         return userDataRepository.findById(concernRoleId)
             .orElseThrow(() -> new ResourceNotFoundException("UserData","concernRoleId",concernRoleId));
     }
-
-  /**  @RequestMapping(method = RequestMethod.GET)
-    public List<UserData> getFirstName(@RequestParam(value="first_name") String firstName) {
-        return userDataRepository.findByFirstName(firstName);
-    }*/
-
-   /**
-    @RequestMapping("/dynamicSearch/{first_name}")
-    public List<UserData> findAllByCriteria(@PathVariable(value="first_name") String firstName) {
-     //   Specification<UserData> spec = UserSpecification.firstNameContains(firstName);
-     //   List<UserData> list = userDataRepository.findAll(Specification.where(spec));
-        return (List<UserData>)userSpec.findByCriteria(firstName, "Thi");
-
-        //return (List<UserData>)userSpec.findByCriteria(null);
-        //return (List<UserData>) userDataRepository.findByFirstName(firstName);
-    }
-
-    @GetMapping("/search1/{first_name}")
-    public List<UserData> getDataByName(@PathVariable(value = "first_name") String firstName) {
-        return (List<UserData>) userDataRepository.findByFirstName(firstName);
-    }*/
 
     @RequestMapping(value="/test", method={RequestMethod.POST})
     public List<UserData> searchClient(@RequestBody @Valid UserSearchCriteria search){
