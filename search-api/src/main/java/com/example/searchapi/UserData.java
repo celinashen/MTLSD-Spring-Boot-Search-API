a1757587c9b209bf56658276cfa6e9e4f4d19a1d
celinashen <celina.shen06@gmail.com>
2020-08-04 09:52:36 -0400

    Update UserData.java

--- search-api/src/main/java/com/example/searchapi/UserData.java
+++ search-api/src/main/java/com/example/searchapi/UserData.java
@@ -1,11 +1,8 @@
 package com.example.searchapi;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//import org.springframework.data.annotation.CreatedDate;
-//import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
-//import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotBlank;
 
 import java.io.Serializable;
@@ -22,10 +19,6 @@ public class UserData implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "concern_role_id")
     private Long concernRoleId;
-    
-
-    //@NotBlank
-    //private String content;
 
     @NotBlank
     @Column(name = "first_name")
